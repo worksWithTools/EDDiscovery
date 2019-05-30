0a594b7008bbb84427da6a20c5b2386882bb673c
worksWithTools <sa_upton@hotmail.com>
2019-05-30 18:56:18 +0100

    Various fixes for EDDDiscovery
    Guid needed pulling in explicitly.
    Pulled files in explicitly.

---
+++ EDDiscovery/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿// The following GUID is for the ID of the typelib if this project is exposed to COM
+using System.Runtime.InteropServices;
+
+[assembly: Guid("1ad84dc2-298f-4d18-8902-7948bccbdc72")]
+
+
